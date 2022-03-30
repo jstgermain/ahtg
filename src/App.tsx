b0a6f4ce7a5738a58f25d8dc5508bd145ccb3a12
Justin St. Germain <jgermain@optimo-it.com>
2022-03-30 15:57:40 -0700

    Added topappbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { Brand } from './components/atoms';
+import { TopAppBar } from './components/organisms';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <TopAppBar />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
--- src/components/organisms/TopAppBar/TopAppBar.tsx
+++ src/components/organisms/TopAppBar/TopAppBar.tsx
@@ -1,11 +1,29 @@
+import { cloneElement } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import CssBaseline from '@mui/material/CssBaseline';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import { common } from '@mui/material/colors';
 import { Brand } from '../../atoms';
 
-export default () => (
-  <AppBar position='relative'>
-    <Toolbar>
-      <Brand size={30} width={243} />
-    </Toolbar>
-  </AppBar>
-);
+export default () => {
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  return (
+    <>
+      <CssBaseline />
+      <AppBar
+        elevation={trigger ? 4 : 0}
+        sx={{ paddingY: trigger ? 0 : 4.25, backgroundColor: common.white }}
+      >
+        <Toolbar>
+          <Brand size={30} width={243} />
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+    </>
+  );
+};
